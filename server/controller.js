c177426f2de73cb478b23d19fa5e16cac93ef384
fdavison1 <fred.noreply@gmail.com>
2019-11-15 12:09:26 -0700

    login complete

--- server/controller.js
+++ server/controller.js
@@ -7,10 +7,10 @@ module.exports = {
         // console.log(username, password)
         const found = await db.find_user({ username })
         console.log(found[0].count)
-        if (found[0].count !== 1) {
+        if (+found[0].count !== 0) {
             return res.status(409).send({ message: 'email already in use' })
         }
-        let  profile_pic  = `https://robohash.org/${username}`
+        let profile_pic = `https://robohash.org/${username}`
         // console.log(profile_pic)
 
 
@@ -21,23 +21,31 @@ module.exports = {
 
         const salt = bcrypt.genSaltSync(10)
         const hash = bcrypt.hashSync(password, salt)
-        db.add_hash( [user_id[0].id, hash] )
+        db.add_hash([user_id[0].id, hash])
         // console.log(user_id[0].id)
 
         req.session.user = { id: user_id[0].id, username, profile_pic }
         res.status(201).send({ message: 'logged in', user: req.session.user })
     },
-
-
-
-
-
-
-
-
     login: async (req, res) => {
         const db = req.app.get('db')
+        const {username, password} = req.body
+        const found = await db.find_user({username})
+        // console.log(found[0].count)
+        if (+found[0].count === 0){
+            return res.status(401).send('user not found')
+        }
 
+        const user = await db.find_hash({username})
+        // console.log(user)
+        const hash = user[0].password
+        // console.log(hash)
+        const goodPassword = bcrypt.compareSync(password, hash)
+        if(!goodPassword){
+            return res.status(403).send('incorrect password')
+        }
+        res.status(200).send(req.session.user)
+        console.log(req.session.user)
 
     }
 }
